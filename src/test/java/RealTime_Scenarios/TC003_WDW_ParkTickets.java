36fd1242f9ee2d60654d9e25762407dabbe92389
kshitijgroup <43273716+kshitijgroup@users.noreply.github.com>
2021-12-29 20:41:04 +0530

    December_29_Changes

--- src/test/java/RealTime_Scenarios/TC003_WDW_ParkTickets.java
+++ src/test/java/RealTime_Scenarios/TC003_WDW_ParkTickets.java
@@ -1,21 +1,50 @@
 package RealTime_Scenarios;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.ChartLocation;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TC003_WDW_ParkTickets {
-    
+
+    ExtentHtmlReporter htmlReporter;
+    static ExtentReports extent;
+    ExtentTest logger;
+
+    @BeforeTest
+    public void startReport() {
+        htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") + "/test-output/ExtentReport.html");
+        extent = new ExtentReports();
+        extent.attachReporter(htmlReporter);
+        extent.setSystemInfo("Host Name", "LocalHost");
+        extent.setSystemInfo("Environment", "Automation Testing");
+        htmlReporter.config().setDocumentTitle("Title of the Report Comes here");
+        htmlReporter.config().setReportName("Name of the Report Comes here");
+        htmlReporter.config().setTestViewChartLocation(ChartLocation.TOP);
+        htmlReporter.config().setTheme(Theme.STANDARD);
+    }
+
     @Test
     public void buyParkTickets() throws Exception {
 
-        //Define Xpaths
+        logger = extent.createTest("TC003_WDW_ParkTickets");
+        // Define Xpaths
         String parksTickets = "//li[@class='parks item flyout']//a[text()='Parks & Tickets']";
         String buyParkTickets = "//li[@class='parks item flyout']//descendant::div//a[text()='Buy Park Tickets']";
         String childCountIncrement = "//div[@class='children small-12 medium-6']//div[@class='selector small-12 medium-12']//descendant::a[@class='plus']";
@@ -28,7 +57,7 @@ public class TC003_WDW_ParkTickets {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
 
-        try{
+        try {
 
             // Launch browser
             driver.get("https://www.disneyholidays.com/walt-disney-world/");
@@ -44,13 +73,13 @@ public class TC003_WDW_ParkTickets {
             Thread.sleep(1000);
             buyParksTicketsLink.click();
 
-            //Add 3 Children
-            WebElement childCount = driver.findElement(By.xpath(childCountIncrement));  
-            for(int i=0;i<3;i++){
+            // Add 3 Children
+            WebElement childCount = driver.findElement(By.xpath(childCountIncrement));
+            for (int i = 0; i < 3; i++) {
                 childCount.click();
                 Thread.sleep(1000);
             }
-            
+
             WebElement numOfDaysBtn = driver.findElement(By.xpath(numOfDays));
             numOfDaysBtn.click();
 
@@ -62,18 +91,23 @@ public class TC003_WDW_ParkTickets {
 
             WebElement actualPrice = driver.findElement(By.xpath(parkHopperPrice));
             String actualTicketPrice = actualPrice.getText();
-            System.out.println("ACTUAL PRICE IS: " +actualTicketPrice);
-            Assert.assertEquals(actualTicketPrice, "1,780.00");
-
-            System.out.println("Ticket prices match successfully");
+            System.out.println("ACTUAL PRICE IS: " + actualTicketPrice);
+            Assert.assertEquals(actualTicketPrice, "2562.00");
+            logger.log(Status.PASS, MarkupHelper.createLabel("buyParkTickets is passTest", ExtentColor.GREEN));
             
             WebElement submit = driver.findElement(By.xpath(submitBtn));
             submit.click();
 
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-        } finally{ 
+        } finally {
             Thread.sleep(3000);
-            driver.quit();}
+            driver.quit();
+        }
+    }
+
+    @AfterClass
+    public static void endTest() {
+        extent.flush();
     }
 }
