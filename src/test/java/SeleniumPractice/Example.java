0d82be326923e456c96341b09c57287250d3a444
kshitijgroup <43273716+kshitijgroup@users.noreply.github.com>
2022-01-28 00:09:32 +0530

    Jan_updates

---
+++ src/test/java/SeleniumPractice/Example.java
@@ -0,0 +1,36 @@
+package SeleniumPractice;
+
+public class Example {
+    
+    public static void main(String[] args) {
+        
+        int arr[] = {-12, 11, -13, -5, 6, -7, 5, -3, -6};
+
+        int low=0;
+        int mid = 0;
+        int high = arr.length-1;
+        while(mid<=high){
+            if(arr[mid]<0){
+                swap(mid, low, arr);
+                low++;
+                mid++;
+            }
+            else if(arr[mid]>0){
+                swap(mid, high, arr);
+                high--;
+            }
+            else{
+                mid++;
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void swap(int low, int high, int arr[]){
+        int temp = arr[low];
+        arr[low] = arr[high];
+        arr[high] = temp;
+    }
+}
---
+++ src/test/java/SeleniumPractice/Example3.java
@@ -0,0 +1,30 @@
+package SeleniumPractice;
+
+public class Example3 {
+    
+    public static void main(String[] args) {
+        
+        int arr[] = {-4,3,-9,0,4,1};
+        float pos = 0;
+        float neg = 0;
+        float zero = 0;
+        float n = arr.length;
+        for(Integer i : arr){
+            if(i>0){
+                pos++;
+            }
+            else if(i<0){
+                neg++;
+            }
+            else{ 
+                zero++;
+            }
+        }
+        System.out.format("%.6f",(pos/n));
+        System.out.println();
+        System.out.printf("%.6f",(neg/n));
+        System.out.println();
+        System.out.printf("%.6f",(zero/n));
+        
+    }
+}
